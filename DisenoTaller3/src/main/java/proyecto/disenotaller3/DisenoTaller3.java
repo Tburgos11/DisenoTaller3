b96faecee1cab5aa86c981b50e155ef899615b12
Tburgos11 <109999494+Tburgos11@users.noreply.github.com>
2024-10-23 19:26:32 -0500

    Cambio de prueba
    1.2

--- DisenoTaller3/src/main/java/proyecto/disenotaller3/DisenoTaller3.java
+++ DisenoTaller3/src/main/java/proyecto/disenotaller3/DisenoTaller3.java
@@ -11,6 +11,6 @@ package proyecto.disenotaller3;
 public class DisenoTaller3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Prueba1");
     }
 }
